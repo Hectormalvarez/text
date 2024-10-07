3f91013e7baf1858ea42914e35687ea622e20499
Hector Alvarez <alvarez.hectorm@outlook.com>
2024-10-06 19:26:22 -0500

    feat: add a Footer component
    This commit adds a new Footer component to the application and updates the layout to include it.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div className="flex flex-col justify-between items-center h-screen">
-      <div>
-        <p>main</p>
-      </div>
-      <div>
-        <p>footer</p>
-      </div>
-    </div>
-  );
+  return <p>main</p>;
 }
