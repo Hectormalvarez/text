8f0d4ed1b9c74492ab7fdb66f43279c406150821
Hector Alvarez <alvarez.hectorm@outlook.com>
2024-11-05 16:39:59 -0600

    ui: fixed page height being larger than window

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,7 +2,7 @@ import Form from "./components/Form";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gray-50">
+    <main className="bg-gray-50">
       <div className="container mx-auto pt-8">
         <h1 className="text-4xl font-bold text-center mb-8 text-gray-800">
           Text Sharing Platform
